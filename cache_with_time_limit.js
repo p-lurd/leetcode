46c7c7a64e8154cac1ceb6fe464de5066007b92b
Jedidiah <obayorit@gmail.com>
2024-12-29 18:31:54 +0100

    problem 2623

--- cache_with_time_limit.js
+++ cache_with_time_limit.js
@@ -61,3 +61,79 @@ TimeLimitedCache.prototype.count = function() {
  * timeLimitedCache.get(1) // 42
  * timeLimitedCache.count() // 1
  */
+
+
+
+
+
+// .......................Not working solution yet...............................
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+    this.cleanupInterval = 5000;
+     this.startPeriodicCleanup();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const currentTime = Date.now();
+    const hasKey = this.cache.has(key) && this.cache.get(key).expiry > currentTime;
+
+    const expiry = currentTime + duration;
+    this.cache.set(key, { value, expiry });
+
+    return hasKey;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key or -1 if expired
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    const currentTime = Date.now();
+    if (this.cache.has(key) && this.cache.get(key).expiry > currentTime) {
+        return this.cache.get(key).value;
+    }
+    return -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    const currentTime = Date.now();
+    this.cleanup(currentTime);
+    return this.cache.size; 
+};
+
+/** 
+ * Cleanup expired keys manually or periodically
+ */
+TimeLimitedCache.prototype.cleanup = function(currentTime) {
+    for (const [key, { expiry }] of this.cache) {
+        if (expiry <= currentTime) {
+            this.cache.delete(key); 
+        }
+    }
+};
+
+/** 
+ * Start periodic cleanup to prevent memory misuse
+ */
+TimeLimitedCache.prototype.startPeriodicCleanup = function() {
+    setInterval(() => {
+        const currentTime = Date.now();
+        this.cleanup(currentTime);
+    }, this.cleanupInterval);
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
