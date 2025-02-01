064a68077aeee1c23c9c1d039973dc4e11b61f6c
Jedidiah <obayorit@gmail.com>
2025-02-01 23:33:38 +0100

    problem 8

--- string_to_integer.js
+++ string_to_integer.js
@@ -1,4 +1,39 @@
-const s = "42";
+const s = "-01042";
+
+
+
+
+// Best solution is this
+var myAtoi = function(s) {
+    const INT_MAX = 2**31 - 1;
+    const INT_MIN = -(2**31);
+
+    let i = 0;
+    let sign = 1;
+    let result = 0;
+    const n = s.length;
+    while (i < n && s[i] === ' ') {
+        i++;
+    }
+    if (i < n && (s[i] === '+' || s[i] === '-')) {
+        sign = (s[i] === '-') ? -1 : 1;
+        i++;
+    }
+    while (i < n && s[i] >= '0' && s[i] <= '9') {
+        let digit = s[i].charCodeAt(0) - '0'.charCodeAt(0);
+        if (result > Math.floor((INT_MAX - digit) / 10)) {
+            return sign === 1 ? INT_MAX : INT_MIN;
+        }
+        
+        result = result * 10 + digit;
+        i++;
+    }
+    
+    return result * sign;
+};
+
+
+
 
 // This doesnt handle negative numbers properly
 var myAtoi = function(s) {
@@ -17,7 +52,7 @@ var myAtoi = function(s) {
     }
     return val;
 };
-    console.log(myAtoi(s));
+    // console.log(myAtoi(s));
 
 
 
@@ -52,4 +87,23 @@ var myAtoi = function(s) {
     return num;
 };
 
+// console.log(myAtoi(s));
+
+
+
+function myAtoi(s) {
+    let val =""
+    for (let i = 0; i<s.length; i++) {
+        if (s[i]===" "){
+            continue;
+        }
+        if(!isNaN(s[i]) || s[i] ==="+" || s[i] === "-"){
+            val += s[i];
+        }
+        else{
+            break;
+        }
+    }
+    return parseInt(val);
+};
 console.log(myAtoi(s));
