bf86f69231dc38ddffede4fbf4cf0aa61d0c5870
Jedidiah <obayorit@gmail.com>
2025-02-07 21:22:13 +0100

    problem 38

---
+++ count_and_say.js
@@ -0,0 +1,7 @@
+var countAndSay = function(n) {
+    if (n === 1) return '1';
+    
+    return countAndSay(n-1)
+        .match(/1+|2+|3+/g)
+        .reduce((acc, nums) => acc += `${nums.length}${nums[0]}`, '')
+};
